8dcd42d0ceb374da172e4f9d7599afc6829e61bf
Sergey Konoplich <entropia.warrior@gmail.com>
2022-04-17 20:11:22 +0300

    feat: mobile menu logic

--- shelter/src/main.js
+++ shelter/src/main.js
@@ -1,1 +1,23 @@
 import './scss/style.scss'
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello world")
+
+  const mobnavBtnOpen  = document.getElementById("mobnav-btn-open")
+  const mobnavOverlay  = document.getElementById("mobnav-overlay")
+  const mobnavBtnClose = document.getElementById("mobnav-btn-close")
+  console.log("open", mobnavBtnOpen)
+  console.log("close", mobnavBtnClose)
+  console.log("open", mobnavOverlay)
+
+
+  mobnavBtnOpen.addEventListener("click", (ev) => {
+    mobnavOverlay.classList.toggle("mobnav-overlay--visible")
+  })
+
+  mobnavBtnClose.addEventListener("click", (ev) => {
+    console.log('test')
+    mobnavOverlay.classList.toggle("mobnav-overlay--visible")
+  })
+
+})
