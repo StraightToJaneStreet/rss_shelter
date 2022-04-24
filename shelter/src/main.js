4149354c295a4dff66ef87847413d98392951504
Sergey Konoplich <entropia.warrior@gmail.com>
2022-04-24 17:19:41 +0300

    feat: mobile menu new structure

--- shelter/src/main.js
+++ shelter/src/main.js
@@ -1,23 +1,66 @@
 import './scss/style.scss'
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello world")
+class MobileMenu {
 
-  const mobnavBtnOpen  = document.getElementById("mobnav-btn-open")
-  const mobnavOverlay  = document.getElementById("mobnav-overlay")
-  const mobnavBtnClose = document.getElementById("mobnav-btn-close")
-  console.log("open", mobnavBtnOpen)
-  console.log("close", mobnavBtnClose)
-  console.log("open", mobnavOverlay)
+  constructor({overlay, button, opened} = { opened: false } ) {
 
+    this.opened = opened
 
-  mobnavBtnOpen.addEventListener("click", (ev) => {
-    mobnavOverlay.classList.toggle("mobnav-overlay--visible")
-  })
+    this.overlayOpenedClass = 'mobnav-overlay--visible'
+    this.buttonRotatedClass = 'mobnav-btn--rotated'
 
-  mobnavBtnClose.addEventListener("click", (ev) => {
-    console.log('test')
-    mobnavOverlay.classList.toggle("mobnav-overlay--visible")
+    this.overlayElement = overlay 
+    this.buttonElement = button
+
+    this.setupHandlers()
+  }
+
+  setupHandlers() {
+    this.buttonElement.addEventListener('click', this.toggle.bind(this))
+    this.overlayElement.addEventListener('click', this.handleOverlayClick.bind(this))
+  }
+
+  handleOverlayClick(e) {
+    if (e.target == e.currentTarget) {
+      this.close() 
+    }
+  }
+
+  open() {
+    console.log('Close menu')
+    this.overlayElement.classList.add(this.overlayOpenedClass)
+    this.buttonElement.classList.add(this.buttonRotatedClass)
+    this.opened = true
+  }
+
+  close() {
+    console.log('Open menu')
+    this.overlayElement.classList.remove(this.overlayOpenedClass)
+    this.buttonElement.classList.remove(this.buttonRotatedClass)
+    this.opened = false
+  }
+
+  toggle() {
+    if (this.opened) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const mobileMenuOverlayElement = document.getElementById('mobnav-overlay')
+  const mobileMenuButtonElement  = document.getElementById('mobnav-btn')
+
+  console.log('mobileMenuOverlayElemnt', mobileMenuOverlayElement)
+  console.log('mobileMenuButtonElement', mobileMenuButtonElement)
+
+  const menu = new MobileMenu({
+    overlay: mobileMenuOverlayElement,
+    button: mobileMenuButtonElement
   })
 
 })
